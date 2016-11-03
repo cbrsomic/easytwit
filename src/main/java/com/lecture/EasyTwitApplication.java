9adc0896276aca38d1af94c53e35a8978b5fc8cc
Redko Rostislav <rredko@connecture.com>
2016-11-03 11:51:45 +0200

    Initial setup

---
+++ src/main/java/com/lecture/EasyTwitApplication.java
@@ -0,0 +1,12 @@
+package com.lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyTwitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyTwitApplication.class, args);
+	}
+}
