e593600470dfaab373abdecfc2fa91a2f3ab64de
Nick Ostroverh <nostroverh@connecture.com>
2017-01-07 17:29:54 +0200

    STEP 5 - Paging

--- src/main/java/com/lecture/repository/PostRepository.java
+++ src/main/java/com/lecture/repository/PostRepository.java
@@ -1,11 +1,19 @@
 package com.lecture.repository;
 
 import com.lecture.domain.Post;
-import com.lecture.domain.projections.OutgoingPost;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.format.annotation.DateTimeFormat;
 
-@RepositoryRestResource(excerptProjection = OutgoingPost.class)
-public interface PostRepository extends CrudRepository<Post, Long>
+import java.time.ZonedDateTime;
+
+public interface PostRepository extends PagingAndSortingRepository<Post, Long>
 {
+  @RestResource(path = "findPostByDate", rel = "findPostByDate")
+  Page findPostByDate(
+      @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) @Param("date") ZonedDateTime date,
+      Pageable p);
 }
