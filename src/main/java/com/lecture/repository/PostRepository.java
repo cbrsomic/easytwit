290e4b073fa79c9e464536c7b260adb9d2792e82
Nick Ostroverh <nostroverh@connecture.com>
2017-01-07 17:28:45 +0200

    STEP 4 - Projections

--- src/main/java/com/lecture/repository/PostRepository.java
+++ src/main/java/com/lecture/repository/PostRepository.java
@@ -1,8 +1,11 @@
 package com.lecture.repository;
 
 import com.lecture.domain.Post;
+import com.lecture.domain.projections.OutgoingPost;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(excerptProjection = OutgoingPost.class)
 public interface PostRepository extends CrudRepository<Post, Long>
 {
 }
